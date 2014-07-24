a962503c24433703a397efc13fc30c549eccf11a
Gabriel Medina <gmedina.santos@gmail.com>
2014-07-24 15:09:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
 
-    <script src="js/functions.js"></script>
+    <script src="js/vendor/functions.js"></script>
     <script src="js/plugins.js"></script>
 
     <script src="js/main.js"></script>
