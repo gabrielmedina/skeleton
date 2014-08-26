84077b2a4912f2b8d8f7c6c3035dad85ba58283d
Gabriel Medina <gmedina.santos@gmail.com>
2014-08-26 13:41:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,28 @@
-<!DOCTYPE html>
+<!doctype html>
 <html class="no-js" lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title></title>
+		<title></title>
 
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" href="img/icon.png">
-    <link rel="stylesheet" href="css/main.css">
+		<link rel="icon" href="img/icon.png">
+		<link rel="stylesheet" href="css/main.css">
 
-    <script src="js/vendor/modernizr.min.js"></script>
-  </head>
+		<script src="js/vendor/modernizr.min.js"></script>
+	</head>
   
-  <body>
-    
+  <body>    
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+		<script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
 
-    <script src="js/vendor/functions.js"></script>
-    <script src="js/plugins.js"></script>
+		<script src="js/vendor/functions.js"></script>
+		<script src="js/plugins.js"></script>
 
-    <script src="js/main.js"></script>
+		<script src="js/main.js"></script>
   </body>
 </html>
